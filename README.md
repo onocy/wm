f311ebb220c742f024c498a378b18214bf27e600
pmichalo <pmichalo@cisco.com>
2018-07-25 12:12:22 -0400

    update

---
+++ README.md
@@ -0,0 +1,29 @@
+# Web Map
+
+A `folium` based html plotter to add visualization data to a static world map. 
+
+### Instructions
+
+1. Clone the repository
+
+> git clone https://github.com/onocy/web-map.git
+
+2. Make sure you have [python 3 installed](https://www.python.org/downloads/mac-osx/)
+
+2. Install the relevant libraries 
+
+> pip install pandas
+> pip install folium
+
+3. Run the python file
+
+> python web-map.py
+
+4. Open the html file
+
+> open Map1.html
+
+---------------------------
+
+![](img/p2.png)
+![](img/p3.png)
--- web-map.py
+++ web-map.py
@@ -1,7 +1,7 @@
 import folium
 import pandas
 
-data = pandas.read_csv("Volcanoes.txt")
+data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
@@ -19,13 +19,13 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
-    fill_color=color_producer(el), fill=True,  color = 'grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = str(el) + "m",
+    fill_color = color_producer(el), fill = True, color = 'grey', fill_opacity = 0.7))
 
-fgp = folium.FeatureGroup(name="Population")
+fgp = folium.FeatureGroup(name = "Population")
 
-fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
-style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fgv)
